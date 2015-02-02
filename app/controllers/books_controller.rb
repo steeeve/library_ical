8452e6de20960c35d8212721791cda4eee4ab080
Stephen Mckellar <stephen@thingmaker.io>
2015-02-02 13:56:06 +0000

    Json root that displays library loans

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,26 @@
+require 'thingmaker/library_loans'
+
 class BooksController < ApplicationController
+  before_action :set_books, only: [:index]
+
   def index
+    respond_to do |format|
+      format.json { render :json => @books }
+    end
   end
+
+  private
+
+    def library_loans
+      @library_loans ||= Thingmaker::LibraryLoans.new(borrower_number)
+    end
+
+    def set_books
+      @books = library_loans.loans
+    end
+
+    def borrower_number
+      params['borrower']
+    end
+
 end
