e5e3403768899a6f643c0594d99c6fe7c4463479
Stephen Mckellar <stephen@thingmaker.io>
2015-02-02 22:05:42 +0000

    Created a homepage.

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,11 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+$(function() {
+  $('#borrower').submit(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var borrower_number = $('#borrower_number').val();
+    $('#calendar_url').val('https://' + location.host + '/books.ics?borrower=' + borrower_number);
+    $('#borrower-calendar').show();
+  });
+});
