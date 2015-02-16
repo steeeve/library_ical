9d11e6abbdcfaee0faf97610c2628159c3b340e0
Stephen Mckellar <stephen@thingmaker.io>
2015-02-16 10:59:53 +0000

    Wrote specs for Loan model

--- spec/lib/thingmaker/the_library/loan_spec.rb
+++ spec/lib/thingmaker/the_library/loan_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 require 'thingmaker/the_library/loan'
 
-describe TheLibrary::Loan do
+describe 'TheLibrary::Loan' do
+
+  let(:loan_fragment) { Nokogiri::HTML.fragment(File.read("spec/fixtures/loan_fragment.html")) }
+  let(:expected_date) { Date.new(2015, 2, 17) }  # This will break in 2016
+
+  subject { TheLibrary::Loan.new(loan_fragment) }
+
+  it 'knows the title' do
+    expect(subject.title).to eq("A Midsummer Night's Dream")
+  end
+
+  it 'knows the author' do
+    expect(subject.author).to eq('Shakespeare, William, 1564-1616')
+  end
+
+  it 'knows the due date' do
+    expect(subject.due_date).to eq(expected_date)
+  end
+
 end
