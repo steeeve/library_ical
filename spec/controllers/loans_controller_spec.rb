0e5d838ce731b44ab1632afa6e771303261c3efd
Stephen Mckellar <stephen@thingmaker.io>
2015-02-16 11:40:38 +0000

    Renamed uncompleted tests to pending until I figure out a way of testing anonymously

--- spec/controllers/loans_controller_spec.rb
+++ spec/controllers/loans_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe LoansController, :type => :controller do
 
   describe "GET index", :vcr do
-    it "returns http success" do
+    pending "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
--- spec/lib/thingmaker/the_library/borrower_spec.rb
+++ spec/lib/thingmaker/the_library/borrower_spec.rb
@@ -3,8 +3,8 @@ require 'thingmaker/the_library/borrower'
 
 describe TheLibrary::Borrower do
 
-  it 'signs in user on initialize'
-
-  it 'parses the accounts page into Loans/Loan objects'
+  # How can we run this test with anonymous data? Use a library other than
+  # VCR?
+  pending 'parses the accounts page into Loans/Loan objects'
 
 end
