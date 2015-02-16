87ccd6f868275a4ca159980743e7957683a890d2
Stephen Mckellar <stephen@thingmaker.io>
2015-02-16 10:58:17 +0000

    Removed the rest of "books" models

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202101933) do
+ActiveRecord::Schema.define(version: 20150216105641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.string   "author"
-    t.date     "due_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
--- spec/factories/books.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :book do
-    title "MyString"
-author "MyString"
-due_date "2015-02-02"
-  end
-
-end
