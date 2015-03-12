f2f48600a16f75984872f83db6c7c1d84c11fa62
Stephen Mckellar <stephen@thingmaker.io>
2015-03-12 12:00:58 +0000

    Fixed loans spec for method rename

--- spec/lib/thingmaker/the_library/loans_spec.rb
+++ spec/lib/thingmaker/the_library/loans_spec.rb
@@ -6,9 +6,9 @@ describe TheLibrary::Loans do
 
   let(:loans_fragment) { Nokogiri::HTML.fragment(File.read("spec/fixtures/loans_fragment.html")) }
 
-  describe '#to_ical' do
+  describe '#to_ics' do
 
-    subject { TheLibrary::Loans.new(loans_fragment).to_ical }
+    subject { TheLibrary::Loans.new(loans_fragment).to_ics }
 
     it 'returns an ical doc with events for each loan' do
       expect(subject.scan('BEGIN:VEVENT').size).to eq(2)
